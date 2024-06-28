ddf4c9ae1fb7841e227656c15e1fe2ecb983c08f
ruicheng <ruicheng@mengxiang.com>
2024-06-28 18:55:33 +0800

    feat:ending part 9

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles['shape']}></div>
+      {/* <div className={styles['shape']}></div> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
